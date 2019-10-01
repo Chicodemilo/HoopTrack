c8b9c153f188cbb22f38e0aaf19137574dc1a4e1
Chicodemilo <mileschick@gmail.com>
2019-10-01 18:38:22 -0500

    Adding checkbox

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { Checkbox } from "react-native-elements";
+import { CheckBox } from "react-native-elements";
 
-const listItem = props => (
-    <TouchableOpacity onPress={props.onItemPressed}>
-        <View style={styles.listItem}>
-            <Text>{props.playerName}</Text>
-            <CheckBox
-                center
-                title="Click Here"
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                checked={this.state.checked}
-            />
-        </View>
-    </TouchableOpacity>
-);
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false
+        };
+    }
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this.props.onItemPressed}>
+                <View style={styles.listItem}>
+                    <Text>{this.props.playerName}</Text>
+                    <CheckBox
+                        center
+                        title="Click Here"
+                        checkedIcon="dot-circle-o"
+                        checked={this.state.checked}
+                        onPress={() => this.setState({ checked: !this.state.checked })}
+                    />
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     listItem: {
@@ -26,4 +37,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default listItem;
+export default ListItem;
